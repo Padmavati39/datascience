b339431e527636c9fd53e0f7f71484f1d39eb4e7
Shubzz-z <smore2630@gmail.com>
2023-12-04 01:27:17 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import Header from './Components/Header';
 import Content from './Components/Content';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
         <NavBar/>
         <Header/>
         <Content/>
+        <Footer/>
     </div>
   );
 }
